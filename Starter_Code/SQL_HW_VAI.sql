0f9cb8f8e7a955f0e37e42f39a2118eac7c30792
Elicitplays <158522959+Elicitplays@users.noreply.github.com>
2024-04-18 23:58:58 -0600

    QUESTIONS DONE
    QUESTIONS ARE DONE

--- Starter_Code/SQL_HW_VAI.sql
+++ Starter_Code/SQL_HW_VAI.sql
@@ -56,8 +56,78 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE to_char(hire_date,'YYYY-MM-DD') LIKE '1986%';
 
---Third
-SELECT a.dept_no, a.emp_no
-FROM 
+--Third list
+CREATE VIEW managers AS
+SELECT dept_manager.dept_no,
+	departments.dept_name, 
+	dept_manager.emp_no
+FROM departments, dept_manager
+WHERE departments.dept_no = dept_manager.dept_no;
 
+SELECT * FROM managers;
 
+SELECT managers.dept_no, 
+	managers.dept_name, 
+	managers.emp_no,
+	employees.last_name, 
+	employees.first_name
+FROM employees, managers
+WHERE managers.emp_no = employees.emp_no;
+
+--4th list
+CREATE VIEW emp_dep AS
+SELECT dept_emp.dept_no,
+	departments.dept_name, 
+	dept_emp.emp_no
+FROM departments, dept_emp
+WHERE departments.dept_no = dept_emp.dept_no;
+
+SELECT * FROM emp_dep;
+
+SELECT emp_dep.dept_no,
+	emp_dep.emp_no,
+	employees.last_name,
+	employees.first_name,
+	emp_dep.dept_name
+FROM emp_dep, employees
+WHERE emp_dep.emp_no = employees.emp_no;
+
+--5th list
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' 
+AND last_name LIKE 'B%';
+
+--6th list
+SELECT emp_no, last_name, first_name
+FROM employees
+WHERE emp_no IN
+(
+	SELECT emp_no
+	FROM dept_emp
+	WHERE dept_no = 'd007'
+);
+
+--7th list
+CREATE VIEW only_dev_and_sales AS
+SELECT emp_no, last_name, first_name
+FROM employees
+WHERE emp_no IN
+(
+	SELECT emp_no
+	FROM dept_emp
+	WHERE dept_no = 'd007' OR dept_no = 'd005'
+);
+SELECT * FROM only_dev_and_sales;
+SELECT * FROM emp_dep;
+
+SELECT o.emp_no, o.last_name, o.first_name, e.dept_name
+FROM emp_dep e, only_dev_and_sales o
+WHERE o.emp_no = e.emp_no 
+AND (e.dept_name = 'Development' or e.dept_name = 'Sales')
+
+--8th list
+SELECT last_name, COUNT(last_name) AS "last name count"
+FROM employees
+GROUP BY last_name
+ORDER BY "last name count" DESC;
